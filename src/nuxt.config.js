6b3a57a2f44a6855378c5aaf24c64132d5cd7a0c
vuongnv <vuongnv@vnpt.vn>
2021-10-26 10:13:26 +0700

    fix build dockerfile

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -134,7 +134,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [    
     '~assets/css/main.css',
-    '@/assets/scss/custom-bootstrap.scss',
+    {src:'@/assets/scss/custom-bootstrap.scss', lang: 'sass'},
     '@/assets/scss/style.scss',
     '@/assets/scss/dashboard.scss',
     '@/assets/scss/question.scss',
