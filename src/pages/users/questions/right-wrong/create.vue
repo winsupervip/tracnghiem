e0135702777b55fbb8624a2eda99876264ee7ed8
ngockhue1620 <ngockhuentca2k@gmail.com>
2021-11-01 23:12:06 +0700

    -right-wrong

--- src/pages/users/questions/right-wrong/create.vue
+++ src/pages/users/questions/right-wrong/create.vue
@@ -1,47 +1,18 @@
 <template>
   <div class="p-question p-question--singleChoice">
     <div class="p-question__left">
-      <Header
-        :question-type="questionType"
-        :get-question="getQuestion"
-        :get-tags="getTags"
-        :get-title="getTitle"
-        :errors="errors"
-      />
-      <AddAnswer
-        :update-value="updateValue"
-        :errors="errors"
-        :update-answer="updateAnswer"
-        :index-answer-update="indexDataUpdate"
-        @add="addListAnswer"
-      />
-      <ListAnswer
-        :list-answers="listAnswers"
-        type-question="single-choice"
-        :update-answer="updateAnswer"
-        :update-right-answer="updateRightAnswer"
-        :errors="errors"
-        :delete-answer="deleteAnswer"
-        @updateListAnswer="updateListAnswer"
-      />
-      <CommentOrNote :get-comment-or-note="getCommentOrNote" />
+      <Header :question-type="questionType" :errors="errors" />
+      <AddAnswer :errors="errors" />
+      <ListAnswer type-question="single-choice" :errors="errors" />
+      <CommentOrNote />
     </div>
     <div class="p-question__right">
-      <PublishQuestion
-        :get-publish-question="getPublishQuestion"
-        :errors="errors"
-        :on-submit="onSubmit"
-      />
-      <Category :get-categories="getCategories" :errors="errors" />
-      <LevelForm :get-level-form="getLevelForm" :errors="errors" />
+      <PublishQuestion :errors="errors" :on-submit="onSubmit" />
+      <Category :errors="errors" />
+      <LevelForm :errors="errors" />
       <!-- <UploadImage :get-image="getImage" /> -->
-      <Uploader
-        v-model="seoAvatar"
-        :accept="'*/*'"
-        :disabled="false"
-        :get-image="getImage"
-      ></Uploader>
-      <AddSeo :get-seo="getSeo" :errors="errors" />
+      <Uploader :accept="'*/*'" :disabled="false"></Uploader>
+      <AddSeo :errors="errors" />
     </div>
   </div>
 </template>
@@ -55,8 +26,9 @@ import {
 } from '@nuxtjs/composition-api'
 // import _ from 'lodash'
 // eslint-disable-next-line no-unused-vars
-import { uuid } from 'vue-uuid'
+// import { uuid } from 'vue-uuid'
 // import EventBus from '../../../../plugins/eventBus'
+import { mapGetters } from 'vuex'
 import PublishQuestion from '@/components/Question/PublishQuestion.vue'
 import LevelForm from '@/components/Question/LevelForm.vue'
 import Category from '@/components/Question/Category.vue'
@@ -86,231 +58,20 @@ export default defineComponent({
   layout: 'dashboard',
   auth: true,
   setup() {
-    const { $logger, i18n } = useContext()
+    const { i18n } = useContext()
     const data = reactive({
       questionType: i18n.t('Thêm câu hỏi đúng sai'),
-      questionContent: '',
-      answerContent: '',
-      options: {
-        convert_urls: false,
-        entity_encoding: 'raw',
-      },
-      isRightAnswer: false,
-      isRandom: false,
-      listAnswers: [
-        {
-          id: uuid.v4(),
-          hashId: '',
-          answerContent: 'Đúng',
-          rightAnswer: 1,
-          random: true,
-          position: 0,
-          plainText: 'Đúng',
-        },
-        {
-          id: uuid.v4(),
-          hashId: '',
-          answerContent: 'Sai',
-          rightAnswer: 0,
-          random: true,
-          position: 0,
-          plainText: 'Sai',
-        },
-        {
-          id: uuid.v4(),
-          hashId: '',
-          answerContent: 'Không có đáp án',
-          rightAnswer: 0,
-          random: true,
-          position: 0,
-          plainText: 'Không có đáp án',
-        },
-      ],
-      indexDataUpdate: -1,
-      categories: [],
-      levelForm: false,
-      statusId: false,
-      levelId: false,
-      seoAvatar: '',
-      seoTitle: '',
-      seoDescription: '',
-      explainationIfCorrect: '',
-      explainationIfInCorrect: '',
-      tags: [],
-      title: '',
-      modalShow: false,
-      updateValue: {},
       errors: [],
-      position: 0,
     })
-    const getQuestion = (value) => {
-      data.questionContent = value
-      $logger.info(value)
-    }
 
     return {
       ...toRefs(data),
-      getQuestion,
     }
   },
+  computed: {
+    ...mapGetters(['isValid', 'getQuestion']),
+  },
   methods: {
-    getCategories(value) {
-      this.categories = value
-      console.log(value)
-    },
-    getLevelForm(value) {
-      this.levelId = value
-      console.log(value)
-    },
-    getPublishQuestion(value) {
-      this.statusId = value
-      console.log(value)
-    },
-    getSeo(value) {
-      this.seoTitle = value.seoTitle
-      this.seoDescription = value.seoDescription
-      console.log(value)
-    },
-    getImage(value) {
-      this.seoAvatar = value
-    },
-    getCommentOrNote(value) {
-      this.explainationIfCorrect = value.explainationIfCorrect
-      this.explainationIfInCorrect = value.explainationIfIncorrect
-      console.log(value)
-      console.log(this.explainationIfCorrect, this.explainationIfInCorrect)
-    },
-    getTags(value) {
-      this.tags = value
-    },
-    getTitle(value) {
-      this.title = value
-    },
-    updateAnswer(value) {
-      if (value === 'remove_data') {
-        this.updateValue = {}
-        this.indexDataUpdate = -1
-      } else {
-        this.indexDataUpdate = this.listAnswers.findIndex(
-          (item) => item.id === value
-        )
-        this.updateValue = this.listAnswers[this.indexDataUpdate]
-      }
-    },
-    updateRightAnswer(value) {
-      this.listAnswers = value
-    },
-    addListAnswer(data) {
-      const value = {
-        answerContent: data.answerContent,
-        random: data.isRandom,
-        rightAnswer: data.isRightAnswer,
-        hashId: '',
-        position: 0,
-        plainText: data.answerContent,
-        id: data.id,
-      }
-      this.listAnswers.push(value)
-      this.$toast.show(this.$i18n.t('Thêm câu trả lời thanh công')).goAway(1500)
-      console.log(this.listAnswers)
-    },
-    updateListAnswer(item) {
-      console.log(3)
-      const answer = this.listAnswers[item.index]
-      answer.answerContent = item.answerContent
-      answer.random = item.isRandom
-      answer.plainText = item.answerContent
-      answer.rightAnswer = item.isRightAnswer
-      console.log(4)
-      this.$toast
-        .show(this.$i18n.t('Cập nhâp câu trả lời thanh công nhé'))
-        .goAway(1500)
-    },
-    deleteAnswer(value) {
-      const index = this.listAnswers.findIndex((item) => item.id === value)
-      this.listAnswers.splice(index, 1)
-    },
-    isValid(data) {
-      // 0
-      this.errors = []
-      let valid = true
-      if (data.question.title === '') {
-        this.errors.push(this.$i18n.t('Tiêu đề là bắt buộc'))
-        valid = false
-      } else {
-        this.errors.push(false)
-      }
-      // 1
-      if (data.question.questionContent === '') {
-        this.errors.push(this.$i18n.t('Bạn phải nhập vào nội dung câu hỏi'))
-        valid = false
-      } else {
-        this.errors.push(false)
-      }
-      // 2
-      if (data.question.tags.length === 0) {
-        this.errors.push(this.$i18n.t('Bạn phải gán ít nhất 1 tag cho câu hỏi'))
-        valid = false
-      } else {
-        this.errors.push(false)
-      }
-      // 3
-      if (!data.question.levelId) {
-        this.errors.push(this.$i18n.t('Bạn phải chọn level cho câu hỏi'))
-        valid = false
-      } else {
-        this.errors.push(false)
-      }
-      // 4
-      if (data.question.categories.length === 0) {
-        this.errors.push(this.$i18n.t('Bạn phải chọn 1 danh mục cho câu hỏi'))
-        valid = false
-      } else {
-        this.errors.push(false)
-      }
-      // 5
-      if (data.answers.length === 0 || data.answers.length > 3) {
-        this.errors.push(
-          this.$i18n.t('Loại câu hỏi này phải có từ 2->3 câu trả lời')
-        )
-        valid = false
-      } else {
-        let count = 0
-        data.answers.forEach((e) => {
-          if (e.rightAnswer === 1) {
-            count += 1
-          }
-        })
-        if (count === 0) {
-          this.$toast
-            .show(this.$i18n.t('Chọn 1 câu trả lời đúng đi'))
-            .goAway(1500)
-          valid = false
-        } else if (count > 1) {
-          this.$toast
-            .show(this.$i18n.t('Loại câu hỏi ni có 1 đáp án thôi'))
-            .goAway(1500)
-          valid = false
-        } else {
-          this.errors.push(false)
-        }
-      }
-      // 6
-      console.log(data.question.statusId)
-      if (!data.question.statusId) {
-        this.errors.push(this.$i18n.t('Bạn có muốn xuất bản câu hỏi'))
-        valid = false
-      } else {
-        this.errors.push(false)
-      }
-      if (data.question.seoTitle === '') {
-        this.errors.push(this.$i18n.t('Bạn có muốn xuất bản câu hỏi'))
-        valid = false
-      } else {
-        this.errors.push(false)
-      }
-      return valid
-    },
     removeAnswerId(value) {
       const listAnswers = value.map((item) => {
         delete item.id
@@ -319,39 +80,19 @@ export default defineComponent({
       return listAnswers
     },
     onSubmit() {
-      console.log('okkkk')
-      const data = {
-        question: {
-          hashId: '',
-          title: this.title,
-          questionTypeId: 3,
-          questionContent: this.questionContent,
-          explainationIfCorrect: this.explainationIfCorrect,
-          explainationIfIncorrect: this.explainationIfInCorrect,
-          statusId: this.statusId,
-          levelId: this.levelId,
-          plainText: this.title,
-          seoAvatar: this.seoAvatar,
-          seoTitle: this.seoTitle,
-          seoDescription: this.seoDescription,
-          tags: this.tags,
-          categories: this.categories,
-          questionGroupId: null,
-          groupOrder: null,
-        },
-        answers: this.listAnswers,
-      }
-      console.log(this.errors)
-      if (this.isValid(data)) {
-        data.answers = this.removeAnswerId(this.listAnswers)
-        this.listAnswers = []
+      const valid = this.isValid
+      this.errors = valid.errors
+      if (valid.isValid) {
+        const data = this.getQuestion
+        data.question.questionTypeId = 3
+        data.answers = this.removeAnswerId(this.getQuestion.answers)
         CauHoiApi.createQuestion(
           data,
           () => {
-            this.$toast.show(this.$i18n.t('Thêm Thành Công')).goAway(1500)
+            this.$toast.show('Thêm Thành Công').goAway(1500)
           },
           () => {
-            this.$toast.show(this.$i18n.t('Có lỗi xảy ra')).goAway(1500)
+            this.$toast.show('Có lỗi xảy ra').goAway(1500)
           }
         )
       }
